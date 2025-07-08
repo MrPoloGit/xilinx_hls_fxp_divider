c6366ef60734ea0aed1f7f8cfd4d5d417d948bbb
Marco Frank <marcofrank@Mac.lan>
2025-07-07 19:51:25 -0700

    Fixed tcl script

--- script.tcl
+++ script.tcl
@@ -3,7 +3,10 @@ set_top divider
 add_files divider.cpp
 add_files -tb testbench.cpp -cflags "-Wno-unknown-pragmas"
 open_solution "solution1"
-set_part {xc7z020clg400-1} # Specify the target FPGA
+
+# Specify the target FPGA
+set_part xc7z020clg400-1
+
 create_clock -period 10 -name default
 csim_design
 csynth_design
