57e00fe6706434b8dfbbe255aaa18c246d8c66ab
MrPoloGit <therealmr.polo1@gmail.com>
2025-07-10 14:44:52 -0700

    Changed target to match ternatry matmul

--- vitis_hls/script.tcl
+++ vitis_hls/script.tcl
@@ -5,7 +5,7 @@ add_files -tb testbench.cpp -cflags "-Wno-unknown-pragmas"
 open_solution "solution1"
 
 # Specify the target FPGA
-set_part xc7z020clg400-1
+set_part xc7a100tcsg324-1
 
 create_clock -period 10 -name default
 csim_design
