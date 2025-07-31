ef007447fed2cc47b1c48a296dc807a68a3c4f00
HSC_Marco <therealmr.polo1@gmail.com>
2025-07-30 20:32:36 -0700

    Current still having issues with dividends and divisor that result in numbers large than 15.875 and -16.0

--- divider.cpp
+++ divider.cpp
@@ -2,36 +2,35 @@
 #include <ap_fixed.h>
 
 // 1 sign + 4 integer + 3 fractional = 8 bits total
-typedef ap_fixed<8, 5> fixed8_t;
+typedef ap_fixed<8, 5> fixed_t;
 static ap_int<8> raw_min = 0b10000000; 
 static ap_int<8> raw_max = 0b01111111;
-static fixed8_t FixedPointMin = *reinterpret_cast<fixed8_t*>(&raw_min); // -16.0
-static fixed8_t FixedPointMax = *reinterpret_cast<fixed8_t*>(&raw_max); // 15.875
+static fixed_t FixedPointMin = *reinterpret_cast<fixed_t*>(&raw_min); // -16.0
+static fixed_t FixedPointMax = *reinterpret_cast<fixed_t*>(&raw_max); // 15.875
 
 void divider(
-    fixed8_t dividend_i,
-    fixed8_t divisor_i,
-    bool     &in_ready_o,     // tells upstream "I'm ready to accept input"
-    bool     in_valid_i,      // tells this module "upstream has valid input"
-    fixed8_t &quotient_o,
-    bool     out_ready_i,     // tells this module "downstream is ready to accept output"
-    bool     &out_valid_o     // tells downstream "I have valid output"
+    fixed_t dividend_i,
+    fixed_t divisor_i,
+    bool    &in_ready_o,
+    bool    in_valid_i,
+    fixed_t &quotient_o,
+    bool    out_ready_i,
+    bool    &out_valid_o
 ) {
     #pragma HLS INTERFACE ap_none port=dividend_i
     #pragma HLS INTERFACE ap_none port=divisor_i
     #pragma HLS INTERFACE ap_none port=in_ready_o
     #pragma HLS INTERFACE ap_none port=in_valid_i
-    #pragma HLS INTERFACE ap_none port=quotient
+    #pragma HLS INTERFACE ap_none port=quotient_o
     #pragma HLS INTERFACE ap_none port=out_ready_i
     #pragma HLS INTERFACE ap_none port=out_valid_o
     #pragma HLS INTERFACE ap_ctrl_none port=return
     #pragma HLS PIPELINE II=1
 
-    static bool valid_internal = false;
-    static fixed8_t result = 0;
+    static fixed_t result = 0;
+    bool valid_internal;
 
-    // Ready to accept input if internal register is not full
-    in_ready_o = !valid_internal;
+    in_ready_o = out_ready_i || !valid_internal;
 
     if (in_valid_i && in_ready_o) {
         // Vitis should be able to automatically figure out and handle this operation
@@ -39,11 +38,12 @@ void divider(
         valid_internal = true;
     }
 
-    quotient = result;
+    quotient_o = result;
     out_valid_o = valid_internal;
 
     // If downstream is ready and output is valid, accept new data next cycle
-    if (out_ready_i && valid_internal) {
+    if (out_ready_i && out_valid_o) {
         valid_internal = false;
     }
+
 }
--- testbench.cpp
+++ testbench.cpp
@@ -5,74 +5,69 @@
 #include <ap_fixed.h>
 
 // 1 sign + 4 integer + 3 fractional = 8 bits total
-typedef ap_fixed<8, 5> fixed8_t;
+typedef ap_fixed<8, 5> fixed_t;
 static ap_int<8> raw_min = 0b10000000; 
 static ap_int<8> raw_max = 0b01111111;
-static fixed8_t FixedPointMin = *reinterpret_cast<fixed8_t*>(&raw_min); // -16.0
-static fixed8_t FixedPointMax = *reinterpret_cast<fixed8_t*>(&raw_max); // 15.875
+static fixed_t FixedPointMin = *reinterpret_cast<fixed_t*>(&raw_min); // -16.0
+static fixed_t FixedPointMax = *reinterpret_cast<fixed_t*>(&raw_max); // 15.875
 
 void divider(
-    fixed8_t dividend_i,
-    fixed8_t divisor_i,
-    bool     &in_ready_o,
-    bool     in_valid_i,
-    fixed8_t &quotient_o,
-    bool     out_ready_i,
-    bool     &out_valid_o
+    fixed_t dividend_i,
+    fixed_t divisor_i,
+    bool    &in_ready_o,
+    bool    in_valid_i,
+    fixed_t &quotient_o,
+    bool    out_ready_i,
+    bool    &out_valid_o
 );
 
-fixed8_t generate_random_fixed8_t() {
-    // uint8_t raw = 0b01111111; // 127
+fixed_t generate_random_fixed_t() {
     uint8_t raw = static_cast<uint8_t>(rand() % 256);
     ap_int<8> signed_val = static_cast<ap_int<8>>(raw);
     std::bitset<8> x(signed_val);
-    fixed8_t fixed_val = *reinterpret_cast<fixed8_t*>(&signed_val);
-    // ap_int<8> raw_bits = *reinterpret_cast<ap_int<8>*>(&fixed_val);
+    fixed_t fixed_val = *reinterpret_cast<fixed_t*>(&signed_val);
     std::bitset<8> y(fixed_val); // it gives the quantized fixed-point value converted to an integer
 
-    std::cout << "generate_random_fixed8_t: raw: " << raw << "\n";
-    std::cout << "generate_random_fixed8_t: signed_val: " << signed_val << "\n";
-    std::cout << "generate_random_fixed8_t: signed_val bitset: " << x << "\n";
-    std::cout << "generate_random_fixed8_t: fixed_val: " << fixed_val << "\n";
-    std::cout << "generate_random_fixed8_t: fixed_val bitset: " << y << "\n";
+    std::cout << "generate_random_fixed_t: raw: "               << raw        << "\n";
+    std::cout << "generate_random_fixed_t: signed_val: "        << signed_val << "\n";
+    std::cout << "generate_random_fixed_t: signed_val bitset: " << x          << "\n";
+    std::cout << "generate_random_fixed_t: fixed_val: "         << fixed_val  << "\n";
+    std::cout << "generate_random_fixed_t: fixed_val bitset: "  << y          << "\n";
 
     return fixed_val;
 }
 
 int main() {
-    fixed8_t dividend_i, divisor_i, quotient_o;
+    fixed_t dividend_i, divisor_i, quotient_o;
+    bool in_ready_o, in_valid_i, out_ready_i, out_valid_o;
     float expected_quotient;
 
-    bool in_valid_i = true;
-    bool in_ready_o;
-    bool out_ready_i = true;
-    bool out_valid_o;
-
-    // Simulate multiple clock cycles
-    const int num_tests = 10;
-    for (int cycle = 0; cycle < num_tests; ++cycle) {
-        dividend_i = generate_random_fixed8_t();
-        divisor_i = generate_random_fixed8_t();
+    const int num_tests = 60;
+    for (int test_num = 0; test_num < num_tests; ++test_num) {
+        std::cout << "Test: " << test_num << "\n";
+        // Pre set up
+        dividend_i = 0;
+        divisor_i = 1;
+        in_valid_i = false;
+        divider(dividend_i, divisor_i, in_ready_o, in_valid_i, quotient_o, out_ready_i, out_valid_o);
+        
+        // Input valid information
+        dividend_i = generate_random_fixed_t();
+        divisor_i = generate_random_fixed_t();
+        in_valid_i = true;
         expected_quotient = dividend_i/divisor_i;
         divider(dividend_i, divisor_i, in_ready_o, in_valid_i, quotient_o, out_ready_i, out_valid_o);
 
-        std::cout << "Cycle " << cycle << ":\n";
-        std::cout << "  dividend_i = " << dividend_i << "\n";
-        std::cout << "  divisor_i = " << divisor_i << "\n";
         if (out_valid_o) {
-            std::cout << "  Expected Result = " << expected_quotient << "\n";
-            std::cout << "  Divider Result = " << quotient_o;
+            if (expected_quotient != quotient_o) {
+                std::cout << "  NOT EQUAL   " << "\n";
+            }
+            std::cout << "  Dividend: " << dividend_i        << "\n";
+            std::cout << "  Divisor:  " << divisor_i         << "\n";
+            std::cout << "  Result:   " << quotient_o        << "\n";
+            std::cout << "  Expected: " << expected_quotient << "\n"; 
         }
-        std::cout << std::endl;
-
-        // Temporary measure to prevent a current crash that is occuring
-        if (in_ready_o && in_valid_i) {
-            in_valid_i = false;
-        }
-
-        if (out_valid_o && out_ready_i) {
-            break;
-        }
+        std::cout << "\n";
     }
 
     return 0;
